edb0926c89caaac689f5ba8d74f4ebbd763068b2
Keagan <esorts.k990@gmail.com>
2021-01-22 15:57:54 -0500

    successfully used the .find array method to find the user from the user array

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,29 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import EditForm from "./EditForm"
-import PostForm from "./PostForm"
+import EditForm from "./EditForm";
 
 const Users = () => {
-
   const [users, setUsers] = useState([]);
 
-  const[name,setName] = useState("")
+  const [name, setName] = useState("");
 
-  const[email,setEmail] = useState("")
+  const [email, setEmail] = useState("");
 
-  const[showEditForm,setShowEditForm] = useState(false)
+  const [showEditForm, setShowEditForm] = useState(false);
 
-  const[currentEditUser,setCurrentEditUser] = useState({})
+  const [currentEditUser, setCurrentEditUser] = useState({});
 
- 
-const recordName = (e) => {
-  setName(e.target.value)
-}
+  const recordName = (e) => {
+    setName(e.target.value);
+  };
 
-const recordEmail = (e) => {
-  setEmail(e.target.value)
-}
-
+  const recordEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -36,16 +32,17 @@ const recordEmail = (e) => {
   console.log(users);
 
   const postUser = async () => {
+  
     const userBody = {
       name: name,
-      email: name
-    }
-    const res = await axios.post("http://localhost:3001/evaluations", userBody)
-    console.log(res)
-  setName("")
-  setEmail("")
-
-  }
+      email: name,
+    };
+    
+    const res = await axios.post("http://localhost:3001/evaluations", userBody);
+    console.log(res);
+    setName("");
+    setEmail("");
+  };
 
   const deleteUser = async (userId) => {
     const res = await axios.delete(
@@ -54,31 +51,56 @@ const recordEmail = (e) => {
     const updatedUsers = users.filter((user) => {
       return user._id !== userId;
     });
-    setUsers(updatedUsers)
+    setUsers(updatedUsers);
     console.log(res);
   };
+// function that shows the EditForm component on button Click and finds a specific user using the .find array method stored in the updatedEditUser
 
   const toggleEditForm = (userId) => {
-setShowEditForm(true)
+    setShowEditForm(true);
+  const updatedEditUser = users.find((user) => {
+return user._id === userId;
+  })
+  setCurrentEditUser(updatedEditUser)
+  console.log(updatedEditUser)
+  };
 
-  }
+  
+
   return (
     <div>
-    <form onSubmit={postUser}>
-    <input type="text" name = "name" placeholder="name" onChange={recordName} value={name} required />
-     <input type="email" name = "email" placeholder="email" onChange={recordEmail} value={email} required/>
-     <button tyep="submit">Post User</button>
-    </form>
-        {showEditForm ? <EditForm /> :
-      users.map((user) => (
-        <div key={user._id}>
-          <p>{user.name}</p>
-          <p>{user.email}</p>
-          <button onClick={() => toggleEditForm(user._id)}>Edit User</button>
-          {/* use empty parenthises and arrow function when passing in parameters */}
-          <button onClick={() => deleteUser(user._id)}>Delete User</button>
-        </div>
-      ))}
+      <form onSubmit={postUser}>
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          onChange={recordName}
+          value={name}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          onChange={recordEmail}
+          value={email}
+          required
+        />
+        <button tyep="submit">Post User</button>
+      </form>
+      {showEditForm ? (
+        <EditForm />
+      ) : (
+        users.map((user) => (
+          <div key={user._id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <button onClick={() => toggleEditForm(user._id)}>Edit User</button>
+            {/* use empty parenthises and arrow function when passing in parameters */}
+            <button onClick={() => deleteUser(user._id)}>Delete User</button>
+          </div>
+        ))
+      )}
     </div>
   );
 };
